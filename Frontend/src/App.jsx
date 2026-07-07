ddd8ad5859fddf79b414f532789abf76ef242f7d
Bluenovich <jegoldigital@gmail.com>
2026-07-07 23:40:30 +0300

    Deployed3

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,9 +7,10 @@ import ChatInput from "./components/ChatInput/ChatInput";
 import "./App.css";
 
 // const API_BASE_URL = "http://localhost:3777/api";
-const API_BASE_URL =
-  `${import.meta.env.VITE_API_URL}/api` ||
-  "https://gizachew-chatgpt-clone.up.railway.app/api";
+const API_BASE_URL = import.meta.env.VITE_API_URL
+  ? `${import.meta.env.VITE_API_URL}/api`
+  : "https://gizachew-chatgpt-clone.up.railway.app/api";
+
 function App() {
   const [conversations, setConversations] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
